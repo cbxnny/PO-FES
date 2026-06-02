bc5dd922594ed8f35a7259d0c9df34a6d1a1cb3a
ohwhennn <owenrasmono@gmail.com>
2026-06-02 21:14:41 +1000

    Created more Tables in the database (Teams, Projects, Units)

--- server/db.js
+++ server/db.js
@@ -18,7 +18,32 @@ const initDb = async () => {
         phone_no TEXT,
         role TEXT NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
+      );
+
+      CREATE TABLE IF NOT EXISTS units (
+        unit_id SERIAL PRIMARY KEY,
+        unit_code TEXT UNIQUE NOT NULL,
+        unit_name TEXT NOT NULL,
+        semester_id INTEGER NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+
+      CREATE TABLE IF NOT EXISTS teams (
+        team_id SERIAL PRIMARY KEY,
+        project_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        team_name TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+
+      CREATE TABLE IF NOT EXISTS projects (
+        project_id SERIAL PRIMARY KEY,
+        unit_id INTEGER NOT NULL,
+        project_name TEXT NOT NULL,
+        description TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+
     `);
     console.log('Database connected and tables ready');
   } catch (err) {
