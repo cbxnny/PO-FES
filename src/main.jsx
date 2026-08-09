5fa882e85918dc7973622dc1a144fcc0e7a9463b
Ben <bennydoan391@gmail.com>
2026-08-09 23:56:03 +1000

    fix main.jsx bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { initAuth } from './utils/auth'
 
-initAuth().finally(() => {
-  createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
-})
+initAuth();
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
