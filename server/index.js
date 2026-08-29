a341e3bcef83082b60592855ba79a89d5ca336ac
jay8999 <n12059099@qut.edu.au>
2026-08-29 07:45:38 +0200

    Update Port

--- server/index.js
+++ server/index.js
@@ -44,4 +44,5 @@ app.get('/api/protected-data', authenticateToken, (req, res) => {
   res.json({ message: 'You have access!', user: req.user });
 });
 
-app.listen(3001, () => console.log('Server running on http://localhost:3001'));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
