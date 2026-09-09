255966e3d95c03bd325b01e445c9f7632ad805a9
owenrasmono <owenrasmono@gmail.com>
2026-09-09 12:19:29 +1000

    Switch escalation emails to Brevo SMTP

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,14 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp-relay.brevo.com',
+  port: 2525,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_APP_PASSWORD
-  },
-  // Necessary to avoid "self signed certificate" errors when using Gmail's SMTP server, adjust when deploying to Render
-  tls: {
-    rejectUnauthorized: false
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD
   }
 });
 
